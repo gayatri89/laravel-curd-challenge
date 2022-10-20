47aa7648b56512744e6a2574a79cd2db6c5deef3
Gayatri <gayatri2889@gmail.com>
2022-10-20 14:28:44 +0200

    layout Templates

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,7 @@
+
+
+		<header class="py-5 bg-stone-200 shadow md:px-6 md:py-8 dark:bg-gray-800 text-center">
+            <div class="flex">
+                <div class="flex-initial w-64"><h2>Orderbird</h2></div>
+            </div>
+		</header>
---
+++ resources/views/layouts/theader.blade.php
@@ -0,0 +1,8 @@
+<thead>
+    <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+    <th class="py-3 px-6 text-left">ID</th>
+    <th class="py-3 px-6 text-left">Firstname</th>
+    <th class="py-3 px-6 text-left">Lastname</th>
+    <th class="py-3 px-6 text-left">Email address</th>
+    </tr>
+</thead> 
